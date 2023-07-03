e7c53a7cb6fbd64982788bacaef28ef3e746d03f
aimee <127796444+aimeedarling@users.noreply.github.com>
2023-07-03 00:21:36 -0700

    added update manager function

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,22 @@
 INSERT INTO department (department_name)
-VALUES ('trash panda'),
+VALUES ('grandma'),
 ('cry baby'),
-('ugly salon'),
-('espresso martini'),
+('hot boiz'),
+('gottacatchthemall'),
 ('grimace');
 
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('daddy tp', 20, 001),
+VALUES ('daddys truck', 20, 001),
 ('master cry-er', 30, 001),
-('uglitician', 50, 003),
-('gma', 15, 002),
-('grimace', 100, 004),
-('pokie-man', 100, 005);
+('gma', 15, 001),
+('grimace', 100, 005),
+('pokie-man', 100, 004);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-('Barla', 'Streetz', 003, null),
-('bulba', 'saur', 004, 001),
-('grimace', 'purple', 005, 002),
-('aimee', 'darling', 006, 003),
-('stinky', 'sebastian', 001, 004);
+('gma', 'g', 001, null),
+('bulba', 'saur', 005, 001),
+('grimace', 'purple', 004, 002),
+('aimee', 'darling', 002, 003),
+('stinky', 'sebastian', 003, 004);
